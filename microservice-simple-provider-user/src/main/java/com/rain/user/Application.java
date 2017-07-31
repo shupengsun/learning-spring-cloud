7597230555d44233b1809c8fa0128452f1fdd20b
shupengsun <sp_sun@163.com>
2017-07-31 21:07:22 +0800

    update

--- microservice-simple-provider-user/src/main/java/com/rain/user/Application.java
+++ microservice-simple-provider-user/src/main/java/com/rain/user/Application.java
@@ -3,6 +3,17 @@ package com.rain.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 监控端点：http://{ip}:{port}/{endpoint}<br>
+ * 比如：<br>
+ * http://127.0.0.1:8000/health<br>
+ * http://127.0.0.1:8000/info<br>
+ * 
+ * @author Rain
+ * @version 1.0
+ * @date 2017年7月31日 下午9:03:37
+ * @since 1.0
+ */
 @SpringBootApplication
 public class Application {
 
