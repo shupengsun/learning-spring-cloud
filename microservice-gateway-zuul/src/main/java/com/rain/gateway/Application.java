18eb59452b42525eda639546b68bf60f6d055d28
shupengsun <sp_sun@163.com>
2017-08-01 14:46:22 +0800

    update

--- microservice-gateway-zuul/src/main/java/com/rain/gateway/Application.java
+++ microservice-gateway-zuul/src/main/java/com/rain/gateway/Application.java
@@ -4,6 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * <h1>微服务网关使用说明</h1>
+ * 
+ * <p>
+ * <strong>依次启动：</strong>
+ * <li>microservice-discovery-eureka</li>
+ * <li>microservice-provider-user</li>
+ * <li>microservice-consumer-goods</li>
+ * <li>microservice-gateway-zuul</li>
+ * <p>
+ * 
+ * <strong>测试地址：</strong>
+ * <li>http://127.0.0.1:9100/microservice-provider-user/login/zhangsan/123456
+ * </li>
+ * <li>http://127.0.0.1:9100/microservice-consumer-goods/goods/get/1</li>
+ * <p>
+ * 
+ * @author rain
+ *
+ */
 @EnableZuulProxy
 @SpringBootApplication
 public class Application {
