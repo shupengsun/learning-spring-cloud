582a91f1680b83fd9a4c3e43b9f254a86f04f889
shupengsun <sp_sun@163.com>
2017-08-01 14:19:39 +0800

    update

--- microservice-discovery-eureka-ha/src/main/java/com/rain/discovery/Application.java
+++ microservice-discovery-eureka-ha/src/main/java/com/rain/discovery/Application.java
@@ -2,7 +2,16 @@ package com.rain.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * --spring.profiles.active=discovery-eureka-a
+ * --spring.profiles.active=discovery-eureka-b
+ * 
+ * @author rain
+ *
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
