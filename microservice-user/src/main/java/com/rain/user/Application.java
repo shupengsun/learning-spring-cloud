aa8c1ebd5534707c5f39ebbdb6c6fdbcb0c4e32e
shupengsun <sp_sun@163.com>
2018-11-20 15:12:16 +0800

    update

--- microservice-user/src/main/java/com/rain/user/Application.java
+++ microservice-user/src/main/java/com/rain/user/Application.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * java -cp microservice-user-0.0.1.jar org.springframework.boot.loader.JarLauncher
+ * 
+ * @author rain
+ *
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
--- microservice-zuul/src/main/java/com/rain/gateway/Application.java
+++ microservice-zuul/src/main/java/com/rain/gateway/Application.java
@@ -10,15 +10,15 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * <p>
  * <strong>依次启动：</strong>
  * <li>microservice-discovery-eureka</li>
- * <li>microservice-provider-user</li>
- * <li>microservice-consumer-goods</li>
- * <li>microservice-gateway-zuul</li>
+ * <li>microservice-user</li>
+ * <li>microservice-goods</li>
+ * <li>microservice-zuul</li>
  * <p>
  * 
  * <strong>测试地址：</strong>
- * <li>http://127.0.0.1:9100/microservice-provider-user/login/zhangsan/123456
+ * <li>http://127.0.0.1:9100/microservice-user/login/zhangsan/123456
  * </li>
- * <li>http://127.0.0.1:9100/microservice-consumer-goods/goods/get/1</li>
+ * <li>http://127.0.0.1:9100/microservice-goods/goods/get/1</li>
  * <p>
  * 
  * @author rain
